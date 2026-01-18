762da5c2ded04059dfd7723db5b2ef414f490af4
ktydg <molokanov02@gmail.com>
2026-01-19 02:54:12 +0300

    refactor: move shadcn css to separate file. Update basic index.css

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
 
-import './styles.css';
+import './index.css';
 
 const rootElement = document.getElementById('app');
 if (rootElement && !rootElement.innerHTML) {
